0d95e569af442f01a7c4fe8e4e43c1b711758c77
lamms06 <s.lammers@belastingdienst.nl>
2021-09-08 11:01:50 +0200

    My first commit

---
+++ src/main/java/org/example/FirstClass.java
@@ -0,0 +1,12 @@
+package org.example; // package name
+
+public class FirstClass { // class name
+
+    //    Fully qualified name (package + class)
+    //    org.example.FirstClass
+
+    public static void main(String[] args){
+        System.out.println("String hello world");
+    }
+
+}
